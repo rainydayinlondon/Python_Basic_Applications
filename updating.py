1f7971c3cd36f4bb6741706de5c491258dbf42ab
rainydayinlondon <rainydyinlondon@gmail.com>
2021-03-02 19:00:16 +0300

    Add files via upload

--- updating.py
+++ updating.py
@@ -3,19 +3,63 @@
 Created on Fri Feb 26 18:21:27 2021
 
 @author: Feyza
+Bu eğitim dersinde, listenin en aşağı kısmına append komutu ile değil de , istediğimiz
+kısmına ekleme yapmamızı sağlayan kodlar bulunuyor...
+
 """
 
-# with open("markalar.txt","a") as file:
-#     file.write("6-BMW")
 
 
 
-with open("markalar.txt","r+",encoding="UTF-8") as file:
-    markalar=file.read()
-    markalar="1-Toyota\n" +markalar
-    # file.seek(0)
-    # file.write("1-Toyota")
+# with open("markalar.txt","a") as file:  # append  - dosyanın sonuna append işlemi yapmış oluyoruz..
+#      file.write("6-BMW")
+
+
+
+
+# with open("markalar.txt","r+",encoding="utf-8")as file:
+#     markalar=file.read()
+#     markalar="1-Toyota\n"+markalar
+#     print(markalar)
+#     file.seek(0)
+#     file.write(markalar)
+    
+
+
+with open("markalar.txt","r+",encoding="utf-8")as file:
+    markalar=file.readlines()  #Satır satır okumamızı sağlıyor..
+    markalar.insert(2,"3-Renault\n")
     file.seek(0)
-    file.write(markalar)
+    # for marka in markalar:
+    #     file.write(marka)  #ekrana yazdırdık..
+    file.writelines(markalar)  # Dizinin elemanlarını tek tek yazdır..
+    
+
+
+
+
+
 with open("markalar.txt") as file:
-    print(file.read())
+    print(file.read())   # markalar txt dosyasını açmış olduk...
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
